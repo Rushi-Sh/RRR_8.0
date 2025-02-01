ec9896446ba22d6562314c4fe0abbb53bfb43366
rajatjoe <119100839+rajatjoe@users.noreply.github.com>
2025-02-01 23:15:51 +0530

    tableau

--- app/analytics/tableau/page.jsx
+++ app/analytics/tableau/page.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+"use client"; // Ensures this component runs only on the client in Next.js
 
-function Tableau() {
-  return (
-    <div>Tableau</div>
-  )
-}
+import React, { useEffect, useRef } from "react";
 
-export default Tableau
+const Tableau = () => {
+    const vizUrl = "https://public.tableau.com/views/RoadProblemsinAhmedabad/Dashboard1?:language=en-US&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link";
+
+    return (
+        <div className="w-full min-h-[125vh] flex align-center justify-center bg-gray-900">
+            <iframe
+                src={`${vizUrl}?:showVizHome=no&:embed=true&:toolbar=yes&:animate_transition=yes`}
+                // style={{ width: "100%", height: "100%", border: "none" }}
+                className="min-h-screen w-full border-none pt-20 mx-auto pl-60"
+                allowFullScreen
+                title="Tableau Dashboard"
+            ></iframe>
+        </div >
+    );
+};
+
+export default Tableau;
--- components/universal/Navbar.jsx
+++ components/universal/Navbar.jsx
@@ -92,7 +92,7 @@ const NavBar = () => {
     }, [pathname]);
 
     return (
-        <nav className={`fixed w-full z-50 transition-all duration-300 ${isScrolled ? 'bg-gray-800 shadow-md' : 'bg-transparent'}`}>
+        <nav className={`fixed w-full z-50 transition-all duration-300 ${isScrolled ? 'bg-gray-800 shadow-md' : 'bg-gray-900'}`}>
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between h-16">
                     <Link href='/' className="flex items-center">
