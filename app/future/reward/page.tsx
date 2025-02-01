bd3cdeaaf83c29dab2989f0db8b7d9af7d744946
rajatjoe <119100839+rajatjoe@users.noreply.github.com>
2025-02-01 20:31:51 +0530

    reward streamlit added

--- app/future/reward/page.tsx
+++ app/future/reward/page.tsx
@@ -1,9 +1,21 @@
+import Image from 'next/image'
 import React from 'react'
 
 function reward() {
-  return (
-    <div>reward</div>
-  )
+    return (
+        <div className="flex flex-col items-center p-4 pt-20 bg-gray-800 text-white dark:bg-gray-900">
+            <Image src="/assets/reward-temp.png"
+                width={800}
+                height={400}
+                alt="Reward System" className="mb-4" />
+            <p className="text-center mb-4">
+                This webpage tracks the reward system to enhance user engagement on public welfare apps by providing a reward system to users.
+            </p>
+            <a href="https://leaderboard-urbansnap.streamlit.app/" className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 dark:bg-blue-700 dark:hover:bg-blue-600">
+                Go to Streamlit App
+            </a>
+        </div>
+    )
 }
 
 export default reward
